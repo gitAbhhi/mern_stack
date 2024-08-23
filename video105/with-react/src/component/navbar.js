13711795ad150a2a66c449bb2852828712e8cd53
gitAbhhi <140035168+gitAbhhi@users.noreply.github.com>
2024-08-23 18:49:26 +0530

    practise on video 105

---
+++ video105/with-react/src/component/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import './navbar.css';
+const navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+            <li>home</li>
+            <li>about us</li>
+            <li>contact</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default navbar
