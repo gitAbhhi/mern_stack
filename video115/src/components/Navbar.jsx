cd3cdf6323a90f81e43fcf21165d8fb0bf9ff4b7
gitAbhhi <140035168+gitAbhhi@users.noreply.github.com>
2024-09-29 16:39:05 +0530

    lecture 115

---
+++ video115/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+const Navbar = () => {
+  return (
+    <div>
+      <NavLink className={(e)=>{return e.isActive?"red":""}} to="/"><li>Home</li></NavLink>
+      <NavLink className={(e)=>{return e.isActive?"red":""}} to="/about"><li>About</li></NavLink>
+      <NavLink className={(e)=>{return e.isActive?"red":""}} to="/login"><li>Login</li></NavLink>
+    </div>
+  )
+}
+
+export default Navbar
